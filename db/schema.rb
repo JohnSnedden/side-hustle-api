85f15dd896d426c95c6521075bb8f4a3a65becd0
John Snedden <jsnedden@gmail.com>
2017-11-27 22:09:05 -0500

    Adds 'company' resource
    - resource scaffolded.
    - routes.rb minor update to limit new/edit
    - create/index/show/update/destroy tested successfully via postman

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20171128025851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "examples", force: :cascade do |t|
-    t.text     "text",       null: false
-    t.integer  "user_id",    null: false
+  create_table "companies", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "address1"
+    t.string "address2"
+    t.string "city"
+    t.string "postal_code"
+    t.string "province"
+    t.string "country"
+    t.string "currency"
+    t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_examples_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "examples", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_examples_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "token",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
+    t.string "email", null: false
+    t.string "token", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "companies", "users"
   add_foreign_key "examples", "users"
 end
